fa694aa194cecb06b9087d0352607161eb1068cd
Marius Petcu <dapetcu21@gmail.com>
2012-08-24 15:41:35 +0300

    Initial commit

---
+++ src/grepit/ftpol/FtPolActivity.java
@@ -0,0 +1,16 @@
+package grepit.ftpol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FtPolActivity extends Activity {
+	
+	private RootView view;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        view = new RootView(this);
+        setContentView(view);
+    }
+}
---
+++ src/grepit/ftpol/RootView.java
@@ -0,0 +1,33 @@
+package grepit.ftpol;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class RootView extends SurfaceView implements SurfaceHolder.Callback  {
+
+	public RootView(Context context) {
+		super(context);
+		
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
