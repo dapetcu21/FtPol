7dd97eb4121859936e80de77617e44d5be167886
Marius Petcu <dapetcu21@gmail.com>
2012-08-24 15:59:47 +0300

    Forgot some stuff

--- src/grepit/ftpol/RootView.java
+++ src/grepit/ftpol/RootView.java
@@ -11,20 +11,17 @@ public class RootView extends SurfaceView implements SurfaceHolder.Callback  {
 		
 	}
 
-	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
 		
